12e3d0de27f7c0deb57e63f709c92ed540f1021c
cyt <cyt@mailtech.cn>
2015-11-01 17:33:45 +0800

    init project

---
+++ ara/rapidjson.h
@@ -0,0 +1,9 @@
+
+#ifndef ARA_RAPIDJSON_H
+#define ARA_RAPIDJSON_H
+
+#include "../3rd/rapidjson/include/rapidjson/reader.h"
+#include "../3rd/rapidjson/include/rapidjson/writer.h"
+
+#endif // ARA_RAPIDJSON_H
+
---
+++ ara/utf8.h
@@ -0,0 +1,7 @@
+
+#ifndef ARA_UTF8_H
+#define ARA_UTF8_H
+
+#include "../3rd/utf8/source/utf8.h"
+
+#endif // ARA_UTF8_H
